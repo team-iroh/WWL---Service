e78a1533c76bacf69b1b12fe70cb681b5dc95496
Warner Lin <WarnerWeiLin@gmail.com>
2020-07-01 20:27:11 -0700

    Updated to Render based on ids in URL

--- public/index.jsx
+++ public/index.jsx
@@ -11,7 +11,7 @@ class Index extends React.Component {
   constructor(props) {
     super (props)
     this.state = {
-      id: 1,//This is the default id... but how do we set it to render based on the campaign?
+      id: 1, //This is the default id... but how do we set it to render based on the campaign?
       updates: {},
       comments: []
     }
@@ -20,24 +20,76 @@ class Index extends React.Component {
 
   //Updates only show up on a specific campaign so the campaign ID will need to be passed in. Assuming each campaign has only one update, I am using the campaigne/s primary key as the id for update as well.
   componentDidMount() {
+    //Old Code
+    // $.ajax({
+    //   type: 'GET',
+    //   url: `/api/update/${this.state.id}`
+    // }).then((results) =>{
+    //   this.setState({
+    //     updates: results
+    //   });
+    //   $.ajax({
+    //     type: 'GET',
+    //     url: `/api/comment/${results.id}`
+    //   }).then((res) =>{
+    //     this.setState({
+    //       comments: res
+    //     });
+    //   });
+    // });
+
+
+    // Attempt 1
+    // const serverAddr = 'http://localhost:3001';
+
+    // const getParams = () => {
+    //   const currentAddress = window.location.href;
+    //   const n = currentAddress.lastIndexOf('/');
+    //   this.setState({
+    //     id: currentAddress.substring(n + 1)
+    //   });
+    // };
+
+    // console.log(window.location.href);
+
+
+    // $.ajax({
+    //   type: 'GET',
+    //   url: `${serverAddr}/api/update/${this.state.id}`
+    // }).then((results) =>{
+    //   this.setState({
+    //     updates: results
+    //   });
+
+    //   $.ajax({
+    //     type: 'GET',
+    //     url: `${serverAddr}/api/comment/${results.id}`
+    //   }).then((res) =>{
+    //     this.setState({
+    //       comments: res
+    //     });
+    //   });
+    // });
+
+
+
+    // Attempt 2
     $.ajax({
       type: 'GET',
-      url: `/api/update/${this.state.id}`
-    })
-    .then((results) =>{
+      url: `http://localhost:3001/api/update${window.location.pathname}`
+    }).then((results) =>{
       this.setState({
         updates: results
-      })
+      });
       $.ajax({
         type: 'GET',
-        url: `/api/comment/${results.id}`
-      })
-      .then((res) =>{
+        url: `http://localhost:3001/api/comment/${results.id}`
+      }).then((res) =>{
         this.setState({
           comments: res
-        })
-      })
-    })
+        });
+      });
+    });
   }
 
   send (thing) {
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path');
+
 const port = 3001
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
@@ -11,13 +13,19 @@ const db = require('../db/index.js');
 
 app.use(cors())
 app.use(morgan())
-app.use('/', express.static('dist'))
+// app.use('/', express.static('dist'));
+app.use('/:id', express.static(path.join(__dirname, '../dist')));
+
+// require('dotenv').config({ path: path.resolve(__dirname, './config/.env') });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 ///////////////////////////////////
 ////////////Routes/////////////////
 ///////////////////////////////////
+
+
 
 // to do Test the code to see if it works
 app.get('/api/update/:id', function (req, res) {
