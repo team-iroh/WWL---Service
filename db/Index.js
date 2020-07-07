97a58630ac754b319602432e62636bf60a2f6bdf
Warner Lin <WarnerWeiLin@gmail.com>
2020-07-07 08:18:55 -0700

    changed server to reference http://ec2204236252122-gu9hddd.compute-1.amazonaws.com/

--- db/Index.js
+++ db/Index.js
@@ -15,7 +15,7 @@ const { Sequelize, DataTypes, Model } = require('sequelize');
 
 
 const sequelize = new Sequelize('kickstarter', 'root', null, {
-  host: 'http://ec2204236252122-gu9hddd.compute-1.amazonaws.com/',
+  host: 'http://ec2204236252122-gu9hddd.compute-1.amazonaws.com',
   dialect: 'mysql'
 });
 
